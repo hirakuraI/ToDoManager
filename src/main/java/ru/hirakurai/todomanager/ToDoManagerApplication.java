e9d5f166d1060f3c1fd76ea0489f71b899d748ba
hirakuraI <arsenchic@mail.ru>
2022-04-30 16:56:22 +0300

    Initial commit

---
+++ src/main/java/ru/hirakurai/todomanager/ToDoManagerApplication.java
@@ -0,0 +1,13 @@
+package ru.hirakurai.todomanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoManagerApplication.class, args);
+    }
+
+}
